53024f21052d01f48b96a7eaafc21511251f0e67
yingtingyang <yangy389@newschool.edu>
2017-02-06 13:24:18 -0500

    wwe

--- Week2/sketch.js
+++ Week2/sketch.js
@@ -1,27 +1,19 @@
-function setup(){
-  createCanvas(500,500);
-}
+<!DOCTYPE html>
+<html>
+<head>
+<script language="javascript" type="text/javascript" src="libraries/p5.js"></script>
+ !--<script language="javascript" src="libraries/p5.dom.js"></script>
+  !--<script language="javascript" src="libraries/p5.sound.js"></script>
+  <script language="javascript" type="text/javascript" src="sketch.js"></script>
+<script src="sketch.js"></script>
+</head>
+<body>
 
-function draw(){
-  background(0);
-  fill(255);
-  if (touchStarted) {
-     for( var y = 0; y < height; y=y+200) {
-       for( var r= 500; r > 0; r-= 50 ) {
-         stroke(random(255),random(255),random(255));
-         strokeWeight(random(5,50));
-         fill(random(100,255));
-         ellipse(250,250,r,r);
-       }
-     }
-   } else {
-      for( var y = 0; y < height; y=y+200){
-         for( var r= 500; r > 0; r-= 50 ){
-             stroke(0);
-             strokeWeight(random(5,50));
-             fill(255);
-             ellipse(250,250,r,r);
-             }
-         }
-      }
-}}
+<h1>Yingting Yang</h1>
+<p> converted sketch</p>
+
+
+
+</body>
+
+</html>
