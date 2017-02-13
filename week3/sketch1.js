33aa645577e635523101a6694d8256ef1ca36971
yingtingyang <yangy389@newschool.edu>
2017-02-13 13:02:02 -0500

    213

--- week3/sketch1.js
+++ week3/sketch1.js
@@ -28,13 +28,6 @@ function draw() {
 
  
    
-      for( var y = 0; y < height; y=y+200){
-         for( var r= 500; r > 0; r-= 50 ){
-             stroke(0);
-             strokeWeight(random(5,50));
-             fill(255);
-             ellipse(250,250,r,r);
-             }
-         }
+     
    
 }
